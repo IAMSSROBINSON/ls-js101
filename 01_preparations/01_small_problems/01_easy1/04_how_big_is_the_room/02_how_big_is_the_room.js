6248a07f5bf58823828913ee35e8a85d5d2a4d4b
SSR <iamssrobinson@gmail.com>
2023-03-22 15:27:09 +0000

    Further exploration file created for exercise. Next: Perform exercise.

---
+++ 01_preparations/01_small_problems/01_easy1/04_how_big_is_the_room/02_how_big_is_the_room.js
@@ -0,0 +1,2 @@
+// Further Exploration
+// Modify the program so that it asks the user for the input type (meters or feet). Compute for the area accordingly, and log it and its conversion in parentheses.
