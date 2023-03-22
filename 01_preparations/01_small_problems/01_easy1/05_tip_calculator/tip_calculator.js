0da5302387b6a917cf0439e421c98ad64ac8ed20
SSR <iamssrobinson@gmail.com>
2023-03-22 16:29:54 +0000

    Set up file for new exercise. Next: Perform exercise.

---
+++ 01_preparations/01_small_problems/01_easy1/05_tip_calculator/tip_calculator.js
@@ -0,0 +1,10 @@
+// Tip Calculator
+// Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip, and then log both the tip and the total amount of the bill to the console. You can ignore input validation and assume that the user will enter numbers.
+
+// Example:
+
+// What is the bill? 200
+// What is the tip percentage? 15
+
+// The tip is $30.00
+// The total is $230.00
