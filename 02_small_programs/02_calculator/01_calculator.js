58bd411300139e7d88c4f5fa0c89a48fb1aec7f8
SSR <iamssrobinson@gmail.com>
2023-03-22 11:48:13 +0000

    Outputs updated to display entire calculation.

--- 02_small_programs/02_calculator/01_calculator.js
+++ 02_small_programs/02_calculator/01_calculator.js
@@ -8,23 +8,23 @@ console.log("First number:", number1);
 let number2 = +READLINE.question("\nPlease enter second number: \n");
 console.log("Second number:", number2);
 
-let operators = +READLINE.question("\nSelect an arithmetic operator:\n1: Add\n2: Multiply\n3: Divide\n4: Subtract\n");
+let operators = +READLINE.question("\nSelect an arithmetic operator:\n1: Addition\n2: Multiplication\n3: Division\n4: Subtraction\n");
 
 switch(operators){
 	case 1:
-		console.log(number1 + number2);
+		console.log(`${number1} + ${number2} = ${number1 + number2}`);
 	break;
 
 	case 2:
-		console.log(number1 * number2);
+		console.log(`${number1} * ${number2} = ${number1 * number2}`);
 	break;
 
 	case 3:
-		console.log(number1 / number2);
+		console.log(`${number1} / ${number2} = ${number1 / number2}`);
 	break;
 
 	case 4:
-		console.log(number1 - number2);
+		console.log(`${number1} - ${number2} = ${number1 - number2}`);
 	break;
 
 	default:
