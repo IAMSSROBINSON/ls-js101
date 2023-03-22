fe3fe6244a2a4091cf90b79c8740f37733760ae9
SSR <iamssrobinson@gmail.com>
2023-03-22 14:27:24 +0000

    Minor function formatting amendments.

--- 01_preparations/01_small_problems/01_easy1/02_odd_numbers/01_odd_numbers.js
+++ 01_preparations/01_small_problems/01_easy1/02_odd_numbers/01_odd_numbers.js
@@ -1,7 +1,7 @@
 // Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
 
 console.log("\nOdd numbers from 1 - 99\n");
-for(let i = 1; i <= 99; i++){
+for (let i = 1; i <= 99; i++) {
 	if(i % 2 === 1){
 		console.log(`${i}`);
 	}
--- 01_preparations/01_small_problems/01_easy1/02_odd_numbers/02_odd_numbers.js
+++ 01_preparations/01_small_problems/01_easy1/02_odd_numbers/02_odd_numbers.js
@@ -5,6 +5,6 @@ const readlineSync = require('readline-sync');
 
 let upperBound = readlineSync.question("\nThis program will print odd numbers from 1 to a specified number (each on a new line). Please specify a number for the upper bound limit:\n");
 
-for(let i = 1; i <= upperBound; i += 2){
+for (let i = 1; i <= upperBound; i += 2) {
 	console.log(i);
 }
