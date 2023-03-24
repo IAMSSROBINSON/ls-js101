a1c1ce2ac21c8371291daef485cfc47d85b14055
SSR <iamssrobinson@gmail.com>
2023-03-24 15:57:30 +0000

    File created for further exploration of the exercise. To be completed later.

---
+++ 01_preparations/01_small_problems/01_easy1/06_sum_or_product_of_consecutive_integers/02_sum_or_product.js
@@ -0,0 +1,2 @@
+// Further Exploration
+// What if the input was an array of integers instead of just a single integer? How would your computeSum and computeProduct functions change? Given that the input is an array, how might you make use of the Array.prototype.reduce() method?
