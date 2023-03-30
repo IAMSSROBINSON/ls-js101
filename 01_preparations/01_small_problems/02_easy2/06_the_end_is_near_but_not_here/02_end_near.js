e0f8a7445f58610f221fa7d7122b514a4dc1270f
SSR <iamssrobinson@gmail.com>
2023-03-30 13:14:57 +0100

    Exercise1 complete. Next: Further exploration considerations.

---
+++ 01_preparations/01_small_problems/02_easy2/06_the_end_is_near_but_not_here/02_end_near.js
@@ -0,0 +1,2 @@
+// Further exploration:
+// Suppose we need a function that retrieves the middle word of a phrase/sentence. What edge cases need to be considered? How would you handle those edge cases without ignoring them? Write a function that returns the middle word of a phrase or sentence. It should handle all of the edge cases you thought of.
