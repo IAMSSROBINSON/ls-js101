6846135f1cdfc48f16339e988c9dc4361ddfdeab
SSR <iamssrobinson@gmail.com>
2023-04-03 14:13:51 +0100

    New feature added to perform new calculation. Next: fix eslint warnings by following modular programming principles.

--- 02_small_programs/12_calculator_bonus_features/01_calculator.js
+++ 02_small_programs/12_calculator_bonus_features/01_calculator.js
@@ -6,60 +6,74 @@ function prompt(msg) {
   console.log(`\n=> ${msg}`);
 }
 
-function invalidNumber (number) {
-
-  return number.trimStart() === '' || Number.isNaN(Number(number));
+function invalidNumber(number) {
+  return number.trimStart() === "" || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to the Calculator!");
+(function intro() {
+  prompt("Welcome to the Calculator!");
 
-prompt("Please enter first number: ");
-let number1 = READLINE.question();
+  prompt("Please enter first number: ");
+  let number1 = READLINE.question();
 
-while (invalidNumber(number1)) {
-  prompt("Invalid number. Enter number value: ");
-  number1 = READLINE.question();
-}
+  while (invalidNumber(number1)) {
+    prompt("Invalid number. Enter number value: ");
+    number1 = READLINE.question();
+  }
 
-prompt("Please enter second number: ");
-let number2 = READLINE.question();
+  prompt("Please enter second number: ");
+  let number2 = READLINE.question();
 
-while (invalidNumber(number2)) {
-  prompt(`Invalid number. Enter number value: `);
-  number2 = READLINE.question();
-}
+  while (invalidNumber(number2)) {
+    prompt(`Invalid number. Enter number value: `);
+    number2 = READLINE.question();
+  }
 
-prompt(
-  "Select an arithmetic operator:\n1: Addition\n2: Multiplication\n3: Division\n4: Subtraction\n"
-);
-let operation = +READLINE.question();
+  prompt(
+    "Select an arithmetic operator:\n1: Addition\n2: Multiplication\n3: Division\n4: Subtraction\n"
+  );
+  let operation = +READLINE.question();
 
-while (![1, 2, 3, 4].includes(operation)) {
-  prompt(`Invalid number. You must choose 1, 2, 3, or 4: `);
-  operation = +READLINE.question();
-}
+  while (![1, 2, 3, 4].includes(operation)) {
+    prompt(`Invalid number. You must choose 1, 2, 3, or 4: `);
+    operation = +READLINE.question();
+  }
 
-let output;
-switch (operation) {
-  case 1:
-    output = Number(number1) + Number(number2);
+  let output;
+  switch (operation) {
+    case 1:
+      output = Number(number1) + Number(number2);
+      break;
+
+    case 2:
+      output = Number(number1) * Number(number2);
+      break;
+
+    case 3:
+      output = Number(number1) / Number(number2);
+      break;
+
+    case 4:
+      output = Number(number1) - Number(number2);
+      break;
+
+    default:
+      prompt("\nInvalid selection.\nEnd of program.");
+      break;
+  }
+
+  prompt(`The result is ${output}\n`);
+  
+
+  let answer;
+  prompt("Do you want to perform new calculation?\n1: yes\n2: no");
+  answer = +READLINE.prompt();
+
+  switch(answer) {
+    case 1:
+      intro();
+      break;
+    case 2: 
     break;
-
-  case 2:
-    output = Number(number1) * Number(number2);
-    break;
-
-  case 3:
-    output = Number(number1) / Number(number2);
-    break;
-
-  case 4:
-    output = Number(number1) - Number(number2);
-    break;
-
-  default:
-    prompt("\nInvalid selection.\nEnd of program.");
-    break;
-}
-
-prompt(`The result is ${output}\n`);
+  }
+})();
