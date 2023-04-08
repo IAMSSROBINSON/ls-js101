e083f5933d165304764762a7d203bf33c205b01b
SSR <iamssrobinson@gmail.com>
2023-04-08 11:39:27 +0100

    File for Further exploration added. Next: Perform further exploration.

---
+++ 01_preparations/01_small_problems/02_easy2/09_how_old_is_teddy/03_how_old_is_teddy.js
@@ -0,0 +1,4 @@
+// Further Exploration
+// The randomBetween function used the Math.floor() method. Would it make a difference if the Math.round() method was used instead?
+
+// Also, how can we make the function more robust? What if the user inadvertently gave the inputs in reverse order (i.e., the value passed to min was greater than max)?
