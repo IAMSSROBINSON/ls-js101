5b3522614bf26c9037c2cf42e0837331a515cee1
SSR <iamssrobinson@gmail.com>
2023-04-09 13:50:43 +0100

    Alternative solution using substring(startIndex, upToNotIncludingIndex) method.

---
+++ 01_preparations/01_small_problems/02_easy2/11_get_middle_character/02_get_middle_character.js
@@ -0,0 +1,18 @@
+// Alternative using values to inform substring(startIndex, upToNotIncludingEndIndex) method
+// substring does not change the original string
+
+function centerOf(string) {
+  if (string.length % 2 === 1) {
+    let centerIndex = (string.length - 1) / 2;
+    return string[centerIndex];
+  } else {
+    let leftIndex = string.length / 2 - 1;
+    return string.substring(leftIndex, leftIndex + 2);
+  }
+}
+
+console.log(centerOf("I Love JavaScript")); // "a"
+console.log(centerOf("Launch School")); // " "
+console.log(centerOf("Launch")); // "un"
+console.log(centerOf("Launchschool")); // "hs"
+console.log(centerOf("x")); // "x"
