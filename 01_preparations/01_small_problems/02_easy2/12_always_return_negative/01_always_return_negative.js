a44cf5613e9a6c82511b9e4178deb746df485587
SSR <iamssrobinson@gmail.com>
2023-04-09 17:18:54 +0100

    Math.abs() turns any numbers absolute value to  positive.

--- 01_preparations/01_small_problems/02_easy2/12_always_return_negative/01_always_return_negative.js
+++ 01_preparations/01_small_problems/02_easy2/12_always_return_negative/01_always_return_negative.js
@@ -5,3 +5,17 @@
 // negative(5);     // -5
 // negative(-3);    // -3
 // negative(0);     // -0
+
+// Math.abs() turns all numbers into positive numbers.
+// Multiplying any positive number by -1 turns the number into a negative number, even 0.
+
+function negative(number) {
+  
+  return Math.abs(number) * -1;
+}
+
+console.log(negative(5)); // -5
+console.log(negative(-3)); // -3
+console.log(negative(0)); // -0
+console.log(negative(-0)); // -0
+
