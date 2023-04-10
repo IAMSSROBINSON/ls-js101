b5468977f60c43e2af0cae5c2114bf7fb088788b
SSR <iamssrobinson@gmail.com>
2023-04-10 17:14:29 +0100

    Validation checks on first promptchecks for 0, string, empty string, -0.

--- 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
+++ 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
@@ -4,9 +4,28 @@ const MESSAGES = require("./messages.json");
 // require readline-sync into the program
 const READLINE_SYNC = require("readline-sync");
 
+// FUNCTION DEFINITIONS
 // printer function
 function printer (message) {
   console.log(`\n=> ${message}`);
 }
 
+// performs validation checks
+function positiveIntValue (value) {
+  return value <= 0 || Number.isNaN(Number(value)) || String(value).trimStart() === "" || Object.is(value, -0);
+}
+
+// declare variables
+let loanAmount; // INPUT
+
+// FUNCTION INVOCATIONS
 printer(MESSAGES['introduction']);
+
+printer("Enter loan amount:");
+loanAmount = +READLINE_SYNC.prompt();
+
+while (positiveIntValue(loanAmount)) {
+  printer("Please enter loan amount as a whole number:");
+  loanAmount = +READLINE_SYNC.prompt();
+}
+
