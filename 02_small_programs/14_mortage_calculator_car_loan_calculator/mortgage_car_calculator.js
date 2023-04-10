fea69aa3538ab3c989e5e49f2078afb7d4de6698
SSR <iamssrobinson@gmail.com>
2023-04-10 20:18:14 +0100

    Updated layout for clarity. variable definition and function declarations at the top of the program. Made names of variables shorter to comply with eslint guidelines. program runs again after output if user selects yes or quits is user selects no.

--- 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
+++ 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
@@ -1,13 +1,31 @@
 // DEPENDENCIES
+
 // require json messages into the program
 const MESSAGES = require("./messages.json");
 
 // require readline-sync into the program
 const READLINE_SYNC = require("readline-sync");
 
-// ---------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-// FUNCTION DEFINITIONS
+// VARIABLE DECLARATIONS
+
+const MONTHS_IN_ONE_YEAR = 12;
+let loanAmt; // INPUT, OUTPUT
+let loanDurationInYears; // INPUT, OUTPUT
+let annualRateOfInterest; // INPUT, OUTPUT
+let aprDecimal;
+let IntPerM;
+let loanMnths;
+let repayPerM; // OUTPUT
+let totalAmountRepayable; // OUTPUT
+let newCalculationAnswer;
+let setNewCalculation = true;
+
+// -----------------------------------------------------------------------------
+
+// FUNCTION DECLARATIONS
+
 // printer function
 function printer (message) {
   console.log(`\n=> ${message}`);
@@ -26,99 +44,90 @@ function toCurrency (value) {
   }).format(`${value}`);
 }
 
-// ---------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-// VARIABLE DECLARATIONS
-const MONTHS_IN_ONE_YEAR = 12;
+while (setNewCalculation === true) {
 
-let loanAmount; // INPUT, OUTPUT
-let loanDurationInYears; // INPUT, OUTPUT
-let annualRateOfInterest; // INPUT, OUTPUT
-let aprDecimal;
-let monthlyInterestRateDecimal;
-let loanDurationInMonths;
-let monthlyRepayment; // OUTPUT
-let totalAmountRepayable; // OUTPUT
-let newCalculationAnswer;
+  // FUNCTION INVOCATIONS
+  // prints introductory message
+  printer(MESSAGES['introduction']);
 
-// ---------------------------------------------------------------------------
+  // get loan amount
+  printer(MESSAGES['loanAmount']);
+  loanAmt = +READLINE_SYNC.prompt();
 
-// FUNCTION INVOCATIONS
-// prints introductory message
-printer(MESSAGES['introduction']);
+  // validate loan amount input
+  while (notPositiveIntValueValidation(loanAmt)) {
+    printer(MESSAGES['valueErrorValidation']);
+    loanAmt = +READLINE_SYNC.prompt();
+  }
 
-// get loan amount
-printer(MESSAGES['loanAmount']);
-loanAmount = +READLINE_SYNC.prompt();
-
-// validate loan amount input
-while (notPositiveIntValueValidation(loanAmount)) {
-  printer(MESSAGES['valueErrorValidation']);
-  loanAmount = +READLINE_SYNC.prompt();
-}
-
-//get loan duration in years
-printer(MESSAGES['loanDurationInYears']);
-loanDurationInYears = +READLINE_SYNC.prompt();
-
-// validate loan duration input
-while (notPositiveIntValueValidation(loanDurationInYears)) {
-  printer(MESSAGES['valueErrorValidation']);
+  //get loan duration in years
+  printer(MESSAGES['loanDurationInYears']);
   loanDurationInYears = +READLINE_SYNC.prompt();
-}
 
-// get annual rate of interest
-printer(MESSAGES['annualRateOfInterest']);
-annualRateOfInterest = +READLINE_SYNC.prompt();
+  // validate loan duration input
+  while (notPositiveIntValueValidation(loanDurationInYears)) {
+    printer(MESSAGES['valueErrorValidation']);
+    loanDurationInYears = +READLINE_SYNC.prompt();
+  }
 
-// validate annual rate of interest input
-while (notPositiveIntValueValidation(annualRateOfInterest)) {
-  printer(MESSAGES['valueErrorValidation']);
+  // get annual rate of interest
+  printer(MESSAGES['annualRateOfInterest']);
   annualRateOfInterest = +READLINE_SYNC.prompt();
-}
 
-// ---------------------------------------------------------------------------
+  // validate annual rate of interest input
+  while (notPositiveIntValueValidation(annualRateOfInterest)) {
+    printer(MESSAGES['valueErrorValidation']);
+    annualRateOfInterest = +READLINE_SYNC.prompt();
+  }
 
-// initialize variables expressions
-aprDecimal = annualRateOfInterest / 100;
+  // ---------------------------------------------------------------------------
 
-monthlyInterestRateDecimal = aprDecimal / MONTHS_IN_ONE_YEAR;
+  // initialize variables expressions
+  aprDecimal = annualRateOfInterest / 100;
 
-loanDurationInMonths = loanDurationInYears * MONTHS_IN_ONE_YEAR;
+  IntPerM = aprDecimal / MONTHS_IN_ONE_YEAR;
 
-monthlyRepayment = loanAmount * (monthlyInterestRateDecimal / (1 - Math.pow((1 + monthlyInterestRateDecimal), (-loanDurationInMonths))));
+  loanMnths = loanDurationInYears * MONTHS_IN_ONE_YEAR;
 
-totalAmountRepayable = monthlyRepayment * loanDurationInMonths;
+  repayPerM = loanAmt * (IntPerM / (1 - Math.pow((1 + IntPerM), (-loanMnths))));
 
-// ---------------------------------------------------------------------------
+  totalAmountRepayable = repayPerM * loanMnths;
 
-// output: print values
-console.log("\n");
-printer(MESSAGES['results_output'].toUpperCase());
-printer(MESSAGES['loanAmount_output'] + `${toCurrency(loanAmount)}`);
-printer(MESSAGES['loanDurationInYears_output'] + `${loanDurationInYears} years`);
-printer(MESSAGES['annualRateOfInterest_output'] + `${annualRateOfInterest}%`);
-printer(MESSAGES['monthlyRepayment_output'] + `${toCurrency(monthlyRepayment)}`);
-printer(MESSAGES['totalAmountRepayable_output'] + `${toCurrency(totalAmountRepayable)}`);
+  // ---------------------------------------------------------------------------
 
-console.log("\n");
-printer(MESSAGES['newCalculation']);
-newCalculationAnswer = READLINE_SYNC.prompt();
+  // output: print values
+  console.log("\n");
+  printer(MESSAGES['results_output'].toUpperCase());
+  printer(MESSAGES['loanAmount_output'] + `${toCurrency(loanAmt)}`);
+  printer(MESSAGES['loanDurationInYears_output'] + `${loanDurationInYears} years`);
+  printer(MESSAGES['annualRateOfInterest_output'] + `${annualRateOfInterest}%`);
+  printer(MESSAGES['monthlyRepayment_output'] + `${toCurrency(repayPerM)}`);
+  printer(MESSAGES['totalAmountRepayable_output'] + `${toCurrency(totalAmountRepayable)}`);
 
-while (!["yes", "y", "no", "n"].includes(newCalculationAnswer.toLowerCase()) || newCalculationAnswer.trimStart() === "") {
-  printer(MESSAGES['newCalculationValidation']);
+  // ---------------------------------------------------------------------------
+
+  // RUN PROGRAM AGAIN ?
+  console.log("\n");
+  printer(MESSAGES['newCalculation']);
   newCalculationAnswer = READLINE_SYNC.prompt();
+
+  while (!["yes", "y", "no", "n"].includes(newCalculationAnswer.toLowerCase()) || newCalculationAnswer.trimStart() === "") {
+    printer(MESSAGES['newCalculationValidation']);
+    newCalculationAnswer = READLINE_SYNC.prompt();
+  }
+
+  newCalculationAnswer = newCalculationAnswer[0].toLowerCase();
+  console.log("newCalculationAnswer", newCalculationAnswer);
+
+  switch (newCalculationAnswer) {
+    case 'y':
+      break;
+
+    case 'n':
+      printer(MESSAGES['goodbye']);
+      setNewCalculation = false;
+      break;
+  }
 }
-
-newCalculationAnswer = newCalculationAnswer[0].toLowerCase();
-console.log("newCalculationAnswer", newCalculationAnswer);
-
-switch (newCalculationAnswer) {
-  case 'y':
-    break;
-
-  case 'n':
-    printer(MESSAGES['goodbye']);
-    break;
-}
-
