4a99252d831de943a43ed8282edae6d59c1f9ce6
SSR <iamssrobinson@gmail.com>
2023-04-11 10:21:41 +0100

    Program to remove consecutive double chars from a string.

--- 01_preparations/01_small_problems/03_easy3/01_ddaaiillyy_ddoouubbllee/01_ddaaiillyy_ddoouubbllee.js
+++ 01_preparations/01_small_problems/03_easy3/01_ddaaiillyy_ddoouubbllee/01_ddaaiillyy_ddoouubbllee.js
@@ -1,9 +1,27 @@
 // ddaaiillyy ddoouubbllee
 // Write a function that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
 
-// Examples:
-// crunch('ddaaiillyy ddoouubbllee');    // "daily double"
-// crunch('4444abcabccba');              // "4abcabcba"
-// crunch('ggggggggggggggg');            // "g"
-// crunch('a');                          // "a"
-// crunch('');                           // ""
+function crunch (string) {
+let newString = [];
+
+if (string.trimStart() === "" || string.length === 1 || typeof string !== 'string') {
+  return string;
+}
+
+newString.push(string[0]);
+
+for (let i = 0; i < string.length; i++) {
+  if (string[i] !==  newString[newString.length-1]) {
+    newString.push(string[i]);
+  }
+  
+}
+
+return newString.join("");
+}
+
+console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""
