5081fec210cbca343e4aae5c70c635b0b6841b2f
SSR <iamssrobinson@gmail.com>
2023-04-11 10:25:06 +0100

    Added alternative solution. Setup 03 file to look deeper into the problem. Next: Look into 03 file and explore concepts.

---
+++ 01_preparations/01_small_problems/03_easy3/01_ddaaiillyy_ddoouubbllee/02_ddaaiillyy_ddoouubbllee.js
@@ -0,0 +1,22 @@
+// ALTERNATIVE
+
+function crunch(text) {
+  let index = 0;
+  let crunchText = '';
+
+  while (index <= text.length - 1) {
+    if (text[index] !== text[index + 1]) {
+      crunchText += text[index];
+    }
+
+    index += 1;
+  }
+
+  return crunchText;
+}
+
+console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""
---
+++ 01_preparations/01_small_problems/03_easy3/01_ddaaiillyy_ddoouubbllee/03_further_exploration.js
@@ -0,0 +1,4 @@
+// Further Exploration
+// You may have noticed that the solution continues iterating until index points to the last character in the string, which means that text[index + 1] is beyond the end of the string during the last iteration. Why does it do this? What happens if we stop iterating when index is equal to text.length - 1?
+
+// It's also possible to solve this using regular expressions. For a nice challenge, give this a try with regular expressions. Can you think of any other solutions that don't use regular expressions?
