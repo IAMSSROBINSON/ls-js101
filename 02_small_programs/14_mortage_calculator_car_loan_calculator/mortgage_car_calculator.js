387f41451430daae6da15920576fc79cb4c8bfc7
SSR <iamssrobinson@gmail.com>
2023-04-13 16:37:26 +0100

    refactored for clarity, efficiency and flow.

--- 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
+++ 02_small_programs/14_mortage_calculator_car_loan_calculator/mortgage_car_calculator.js
@@ -1,37 +1,64 @@
-// DEPENDENCIES
-
-// require json messages into the program
 const MESSAGES = require("./messages.json");
-
-// require readline-sync into the program
 const READLINE_SYNC = require("readline-sync");
-
-// -----------------------------------------------------------------------------
-
-// VARIABLE DECLARATIONS
-
 const MONTHS_IN_ONE_YEAR = 12;
-// let loanAmt; // INPUT, OUTPUT
-// let loanDurationInYears; // INPUT, OUTPUT
-// let annualRateOfInterest; // INPUT, OUTPUT
-// let aprDecimal;
-// let intPerM;
-// let loanMnths;
-// let repayPerM; // OUTPUT
-// let totalAmtRepayable; // OUTPUT
-
 let restartProgram = true;
 
 // -----------------------------------------------------------------------------
 
-// FUNCTION DECLARATIONS
+while (restartProgram === true) {
+  getWelcome();
+  let loanAmount = getLoan();
+  let loanDurationInYears = getLoanDurationYears();
+  let aprAmount = getAPR();
+  let aprDecimal = getAPRdecimal(aprAmount);
+  let interestPerMonth = getInterestPerMonth(aprDecimal);
+  let loanDurationMonths = getLoanMonths(loanDurationInYears);
+  let monthlyPayment = getMonthlyPayment(loanAmount,
+    interestPerMonth, loanDurationMonths);
+  let totalRepayable = getTotalAmountRepayable(monthlyPayment,
+    loanDurationMonths);
+  displayFinalResults(
+    loanAmount,
+    loanDurationInYears,
+    aprAmount,
+    monthlyPayment,
+    totalRepayable
+  );
+  askToCalculateAgain();
+}
 
-// printer function
+// -----------------------------------------------------------------------------
+
+function toCurrency(value) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(`${value}`);
+}
+
 function printer(message) {
   console.log(`\n=> ${message}`);
 }
 
-// performs validation checks
+// -----------------------------------------------------------------------------
+
+function getWelcome() {
+  printer(MESSAGES["welcome"]);
+}
+
+function getLoan() {
+  let loanAmount;
+
+  printer(MESSAGES["loanAmount_prompt"]);
+  loanAmount = +READLINE_SYNC.prompt();
+
+  while (intValueValidation(loanAmount)) {
+    printer(MESSAGES["error_int_validation"]);
+    loanAmount = +READLINE_SYNC.prompt();
+  }
+  return loanAmount;
+}
+
 function intValueValidation(value) {
   return (
     value <= 0 ||
@@ -41,7 +68,20 @@ function intValueValidation(value) {
   );
 }
 
-// loan duration validation checks
+function getLoanDurationYears() {
+  let loanDurationInYears;
+
+  printer(MESSAGES["loanDurationInYears_prompt"]);
+  loanDurationInYears = READLINE_SYNC.prompt();
+
+  while (validateLoanDuration(loanDurationInYears)) {
+    printer(MESSAGES["error_wholeNumber_validation"]);
+    loanDurationInYears = READLINE_SYNC.prompt();
+  }
+
+  return Number(loanDurationInYears);
+}
+
 function validateLoanDuration(value) {
   return (
     Number(value) <= 0 ||
@@ -52,7 +92,19 @@ function validateLoanDuration(value) {
   );
 }
 
-// performs validation checks on apr
+function getAPR() {
+  let aprAmount;
+
+  printer(MESSAGES["APR_prompt"]);
+  aprAmount = READLINE_SYNC.prompt();
+
+  while (aprValidation(aprAmount)) {
+    printer(MESSAGES["error_int_validation"]);
+    aprAmount = READLINE_SYNC.prompt();
+  }
+  return Number(aprAmount);
+}
+
 function aprValidation(value) {
   return (
     value < Number(0) ||
@@ -62,63 +114,11 @@ function aprValidation(value) {
   );
 }
 
-// convert loan amount to currency value
-function toCurrency(value) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(`${value}`);
+function getAPRdecimal(aprAmount) {
+  return aprAmount / 100;
 }
 
-// getLoan function
-function getLoan() {
-  let loanAmt;
-
-  printer(MESSAGES["loanAmount"]);
-  loanAmt = +READLINE_SYNC.prompt();
-
-  while (intValueValidation(loanAmt)) {
-    printer(MESSAGES["valueErrorValidation"]);
-    loanAmt = +READLINE_SYNC.prompt();
-  }
-  return loanAmt;
-}
-
-function getLoanDurationYears() {
-  let loanDurationInYears;
-
-  printer(MESSAGES["loanDurationInYears"]);
-  loanDurationInYears = READLINE_SYNC.prompt();
-
-  // validate loan duration input
-  while (validateLoanDuration(loanDurationInYears)) {
-    printer(MESSAGES["valueErrorValidation"]);
-    loanDurationInYears = READLINE_SYNC.prompt();
-  }
-
-  return Number(loanDurationInYears);
-}
-
-// get annual rate of interest
-function getAPR() {
-  let annualRateOfInterest;
-
-  printer(MESSAGES["annualRateOfInterest"]);
-  annualRateOfInterest = READLINE_SYNC.prompt();
-
-  // validate annual rate of interest input
-  while (aprValidation(annualRateOfInterest)) {
-    printer(MESSAGES["valueErrorValidation"]);
-    annualRateOfInterest = READLINE_SYNC.prompt();
-  }
-  return Number(annualRateOfInterest);
-}
-
-function getAPRdecimal(annualRateOfInterest) {
-  return annualRateOfInterest / 100;
-}
-
-function getIntPerMonth(aprDecimal) {
+function getInterestPerMonth(aprDecimal) {
   return aprDecimal / MONTHS_IN_ONE_YEAR;
 }
 
@@ -126,48 +126,50 @@ function getLoanMonths(loanDurationInYears) {
   return loanDurationInYears * MONTHS_IN_ONE_YEAR;
 }
 
-function getRepayPerM(loanAmt, intPerM, loanMnths) {
-  if (intPerM === 0) {
-    return loanAmt / loanMnths;
+function getMonthlyPayment(loanAmount, interestPerMonth, loanDurationMonths) {
+  if (interestPerMonth === 0) {
+    return loanAmount / loanDurationMonths;
   }
 
-  return loanAmt * (intPerM / (1 - Math.pow(1 + intPerM, -loanMnths)));
+  return loanAmount *
+  (interestPerMonth /
+  (1 - Math.pow(1 + interestPerMonth, -loanDurationMonths)));
 }
 
-function getTotalAmountRepayable(repayPerM, loanMnths) {
-  return repayPerM * loanMnths;
+function getTotalAmountRepayable(monthlyPayment, loanDurationMonths) {
+  return monthlyPayment * loanDurationMonths;
 }
 
 function displayFinalResults(
-  loanAmt,
+  loanAmount,
   loanDurationInYears,
-  annualRateOfInterest,
-  repayPerM,
-  totalAmtRepayable
+  aprAmount,
+  monthlyPayment,
+  totalRepayable
 ) {
   console.log("\n");
-  printer(MESSAGES["results_output"].toUpperCase());
-  printer(MESSAGES["loanAmount_output"] + `${toCurrency(loanAmt)}`);
+  printer(MESSAGES["results_title"].toUpperCase());
+  printer(MESSAGES["loanAmount_title"] + `${toCurrency(loanAmount)}`);
   printer(
-    MESSAGES["loanDurationInYears_output"] + `${loanDurationInYears} years`
+    MESSAGES["loanDurationInYears_title"] + `${loanDurationInYears} years`
   );
-  printer(MESSAGES["annualRateOfInterest_output"] + `${annualRateOfInterest}%`);
-  printer(MESSAGES["monthlyRepayment_output"] + `${toCurrency(repayPerM)}`);
+  printer(MESSAGES["apr_title"] + `${aprAmount}%`);
+  printer(MESSAGES["monthlyPayment_title"] + `${toCurrency(monthlyPayment)}`);
   printer(
-    MESSAGES["totalAmtRepayable_output"] + `${toCurrency(totalAmtRepayable)}`
+    MESSAGES["totalRepayable_title"] + `${toCurrency(totalRepayable)}`
   );
 }
 
 function askToCalculateAgain() {
   let newCalculationAnswer;
 
-  printer(MESSAGES["newCalculation"]);
+  printer(MESSAGES["reStartProgram_prompt"]);
   newCalculationAnswer = READLINE_SYNC.prompt();
 
   while (
     !["yes", "y", "no", "n"].includes(newCalculationAnswer.toLowerCase())
   ) {
-    printer(MESSAGES["newCalculationValidation"]);
+    printer(MESSAGES["reStartProgram_validation"]);
     newCalculationAnswer = READLINE_SYNC.prompt();
   }
 
@@ -184,109 +186,5 @@ function askToCalculateAgain() {
   }
 }
 
-function getWelcome() {
-  printer(MESSAGES["introduction"]);
-}
-
 // -----------------------------------------------------------------------------
 
-// MAIN FUNCTIONS
-
-while (restartProgram === true) {
-  // prints introductory message
-
-  // get loan amount
-
-  // validate loan amount input
-  getWelcome();
-  let loanAmt = getLoan();
-  let loanDurationInYears = getLoanDurationYears();
-  let annualRateOfInterest = getAPR();
-  let aprDecimal = getAPRdecimal(annualRateOfInterest);
-  console.log("aprDecimal", aprDecimal);
-  let intPerM = getIntPerMonth(aprDecimal);
-  console.log("intPerM", intPerM);
-  let loanMnths = getLoanMonths(loanDurationInYears);
-  let repayPerM = getRepayPerM(loanAmt, intPerM, loanMnths);
-  let totalAmtRepayable = getTotalAmountRepayable(repayPerM, loanMnths);
-  displayFinalResults(
-    loanAmt,
-    loanDurationInYears,
-    annualRateOfInterest,
-    repayPerM,
-    totalAmtRepayable
-  );
-  askToCalculateAgain();
-}
-
-// while (intValueValidation(loanAmt)) {
-//   printer(MESSAGES['valueErrorValidation']);
-//   loanAmt = +READLINE_SYNC.prompt();
-// }
-
-// get loan duration in years
-// printer(MESSAGES['loanDurationInYears']);
-// loanDurationInYears = +READLINE_SYNC.prompt();
-
-// // validate loan duration input
-// while (intValueValidation(loanDurationInYears)) {
-//   printer(MESSAGES['valueErrorValidation']);
-//   loanDurationInYears = +READLINE_SYNC.prompt();
-// }
-
-// // get annual rate of interest
-// printer(MESSAGES["annualRateOfInterest"]);
-// annualRateOfInterest = +READLINE_SYNC.prompt();
-
-// // validate annual rate of interest input
-// while (intValueValidation(annualRateOfInterest)) {
-//   printer(MESSAGES["valueErrorValidation"]);
-//   annualRateOfInterest = +READLINE_SYNC.prompt();
-// }
-
-// ---------------------------------------------------------------------------
-
-// CALCULATIONS
-
-// initialize variables expressions
-// aprDecimal = annualRateOfInterest / 100;
-
-// intPerM = aprDecimal / MONTHS_IN_ONE_YEAR;
-
-// loanMnths = loanDurationInYears * MONTHS_IN_ONE_YEAR;
-
-// repayPerM = loanAmt * (intPerM / (1 - Math.pow(1 + intPerM, -loanMnths)));
-
-// totalAmtRepayable = repayPerM * loanMnths;
-
-// ---------------------------------------------------------------------------
-
-// OUTPUT RESULTS
-
-// ---------------------------------------------------------------------------
-
-// RESTART PROGRAM ?
-
-//   console.log("\n");
-//   printer(MESSAGES["newCalculation"]);
-//   newCalculationAnswer = READLINE_SYNC.prompt();
-
-//   while (
-//     !["yes", "y", "no", "n"].includes(newCalculationAnswer.toLowerCase())
-//   ) {
-//     printer(MESSAGES["newCalculationValidation"]);
-//     newCalculationAnswer = READLINE_SYNC.prompt();
-//   }
-
-//   newCalculationAnswer = newCalculationAnswer[0].toLowerCase();
-
-//   switch (newCalculationAnswer) {
-//     case "y":
-//       break;
-
-//     case "n":
-//       printer(MESSAGES["goodbye"]);
-//       restartProgram = false;
-//       break;
-//   }
-// }
