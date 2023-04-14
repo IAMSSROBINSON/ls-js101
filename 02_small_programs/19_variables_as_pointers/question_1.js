9d781b374f64463c526a34620329ea1508d352e8
SSR <iamssrobinson@gmail.com>
2023-04-14 12:36:24 +0100

    Question demonstrating Primitive values as independent values stored at their own address in memory.

--- 02_small_programs/19_variables_as_pointers/question_1.js
+++ 02_small_programs/19_variables_as_pointers/question_1.js
@@ -10,4 +10,16 @@ console.log(myOtherWord);
 
 /*
 
+variable: myWord declared and initialized to string value: 'Hello'
+- JavaScript creates a space in memory for the variable
+- the value is stored at that memory address
+
+variable: myOtherWord declared and value: myWord assigned to it
+- JavaScript creates a space in memory for the variable
+- the value that variable: myWord points to is stored at this new address
+
+the console will print:
+myWord => 'Hello'
+myOtherWord => 'Hello'
+
 */
