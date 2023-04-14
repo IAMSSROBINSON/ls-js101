9d6beddd0b3c119cee421ad5bc589e61f06b6097
SSR <iamssrobinson@gmail.com>
2023-04-14 12:57:10 +0100

    arrays are pointers and each have their own memory address.

--- 02_small_programs/19_variables_as_pointers/question_4.js
+++ 02_small_programs/19_variables_as_pointers/question_4.js
@@ -11,4 +11,13 @@ console.log(myOtherWords);
 
 /*
 
+variable: myWords declared and initialized to array object at 1 address
+variable: myOtherWords declared and initialized to value myWords references
+myWords reassigned to reference new array object as new address
+myOtherWords points to address of original array
+
+the console will print:
+myWords => ['Hi', 'Bye']
+myOtherWords => ['Hello', 'Goodbye'];
+
 */
