cfc7f4115e0e8c80ae2ea253b3540a58956e5369
SSR <iamssrobinson@gmail.com>
2023-04-14 13:04:35 +0100

    arrays are not primitive values

--- 02_small_programs/19_variables_as_pointers/question_6.js
+++ 02_small_programs/19_variables_as_pointers/question_6.js
@@ -13,4 +13,13 @@ console.log(myWord);
 
 /*
 
+variable: myWords declared and initialized to array object at 1 address
+new variable: myWord declared and initialized to string: 'Hi'
+myWords array element1 reassigned to value: 'Hi'
+myWord variable reassigned to value: "Hello"
+
+the console will print:
+myWords => ['Hi', 'Goodbye']
+myWord => 'Hello';
+
 */
