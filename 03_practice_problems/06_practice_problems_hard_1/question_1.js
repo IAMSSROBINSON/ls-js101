666d31ee03f0778b873116f15085151b3b056cf2
SSR <iamssrobinson@gmail.com>
2023-04-20 14:57:51 +0100

    Q1 Completed.

---
+++ 03_practice_problems/06_practice_problems_hard_1/question_1.js
@@ -0,0 +1,27 @@
+// Will the code below raise an error?
+
+// let numbers = [1, 2, 3];
+// numbers[6] = 5;
+
+// console.log(numbers[6])
+
+/*
+No it should fill in the elements that have not been defined with undefined but the elements are actually empty
+*/
+
+
+
+// Bonus:
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+numbers[4];  
+// what will this line return? // it should return undefined
+
+console.log(numbers);
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
