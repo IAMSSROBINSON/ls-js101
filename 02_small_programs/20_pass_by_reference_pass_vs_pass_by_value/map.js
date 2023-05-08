ab70a235b16c6b9c505ed3f90f6e3ef1686e8eb4
SSR <iamssrobinson@gmail.com>
2023-05-08 12:55:20 +0100

    File name change.

--- 02_small_programs/20_pass_by_reference_pass_vs_pass_by_value/map.js
+++
@@ -1,9 +0,0 @@
-function capitalize (names) {
-  return names.map((name) => {
-    return name[0].toUpperCase() + name.slice(1);
-  });
-}
-
-let names = ["stephane", "nassima", "emily"];
-console.log(capitalize(names)); // [ 'Stephane', 'Nassima', 'Emily' ]
-console.log(names);             // [ 'stephane', 'nassima', 'emily' ]
