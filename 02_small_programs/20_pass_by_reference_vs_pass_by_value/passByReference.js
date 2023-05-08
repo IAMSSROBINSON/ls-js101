05fc689071e9be63581a83d01c5e0054fab66716
SSR <iamssrobinson@gmail.com>
2023-05-08 12:49:04 +0100

    File name change.

--- 02_small_programs/20_pass_by_reference_vs_pass_by_value/passByReference.js
+++
@@ -1,9 +0,0 @@
-function capitalize (names) {
-  for (let index = 0; index < names.length; index++) {
-    names[index] = names[index][0].toUpperCase() + names[index].slice(1); // function affects original object
-  }
-}
-
-let names = ["chris", "kevin", "stephane"];
-capitalize(names);
-console.log(names); // ["Chris", "Kevin", "Stephane"]
