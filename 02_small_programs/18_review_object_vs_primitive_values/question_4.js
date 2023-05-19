71358645657ac1e93d68b5f5f95c9ab7dd7581ef
SSR <iamssrobinson@gmail.com>
2023-05-19 17:11:56 +0100

    Elaborated on explanation to the answer to the question to be more descriptive.

--- 02_small_programs/18_review_object_vs_primitive_values/question_4.js
+++ 02_small_programs/18_review_object_vs_primitive_values/question_4.js
@@ -1,5 +1,5 @@
 // Question 4
-// What will the following code log to the console and why? 
+// What will the following code log to the console and why?
 // Don't run it until you have tried to answer.
 
 let myWords = ['Hello'];
@@ -12,5 +12,14 @@ console.log(myWords);
 Variable declared and array object with string value inside assigned
 .concat attempts to perform string method on array object variable
 console.log will print original array: ['Hello']
+
+*/
+
+/*
+
+This code outputs the array: ['Hello'].
+The .concat() method used on arrays will return a new array and will not mutate the caller, thus the array that is passed as the value to be concatenated to the caller is not saved to any storage medium be it a new variable or otherwise. 
+
+Therefore when the console.log() method is invoked on the last line of code the value that argument points to is the original array with 1 primitive string value present inside as the only element.
 
 */
