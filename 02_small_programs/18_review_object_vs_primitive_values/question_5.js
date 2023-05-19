ef593a54eeaf40f57740ad1624f5cfb0f37fddc5
SSR <iamssrobinson@gmail.com>
2023-05-19 17:26:47 +0100

    Expounded on my answer description.

--- 02_small_programs/18_review_object_vs_primitive_values/question_5.js
+++ 02_small_programs/18_review_object_vs_primitive_values/question_5.js
@@ -16,3 +16,15 @@ console.log prints original value of variable which is the array
 variable has not been mutated: ['Hello']
 
 */
+
+/*
+
+This code should output: ['Hello']
+
+The .toUpperCase() method invocation on the second line of code returns a new string which does not get saved in this code and thus its value is lost upon the method invocation.
+
+The second line of code uses bracket notation to access the 0 indexed element in the array and mutate it to uppercase but the method is non-mutating and thus the string cannot be mutated rather, a new string is returned with the results of the method applied.
+
+The console.log() method invocation on the last line of code passes in the variable name as a pointer to the array and thus the original array is output to the output stream.
+
+*/
