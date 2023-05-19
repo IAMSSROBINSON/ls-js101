850ca2710d098dd52d6c731e892d2433d9bb8792
SSR <iamssrobinson@gmail.com>
2023-05-19 18:06:58 +0100

    Concatenated a more descriptive answer to the question with the previous answer.

--- 02_small_programs/18_review_object_vs_primitive_values/question_6.js
+++ 02_small_programs/18_review_object_vs_primitive_values/question_6.js
@@ -15,5 +15,14 @@ index 0 of array accessed
 index 0 of array mutated to uppercase characters
 console.log: ['HELLO']
 
+*/
+
+/*
+
+This code should output an array with a uppercase string value as it's 0th indexed element.
+
+On line 2 of the code the pointer variable declared on line one (which points to an array at a different address in memory) is used with bracket notation to reassign its value at the index specified inside the brackets directly to a new string value as returned by the .toUpperCase() method invocation on said array element value.
+
+The console.log() method invocation on the last line of code will therefore output the value of the argument passed, which points to the array with the mutated value of its 0th indexed element.
 
 */
