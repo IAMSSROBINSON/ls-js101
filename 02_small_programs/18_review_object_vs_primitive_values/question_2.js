8b59eeda24f78fcec58830e12a14bc54568244e5
SSR <iamssrobinson@gmail.com>
2023-05-19 16:48:26 +0100

    Updated answer to question to be more of a user level descriptive answer.

--- 02_small_programs/18_review_object_vs_primitive_values/question_2.js
+++ 02_small_programs/18_review_object_vs_primitive_values/question_2.js
@@ -1,5 +1,5 @@
 // Question 2
-// What will the following code log to the console and why? 
+// What will the following code log to the console and why?
 // Don't run it until you have tried to answer.
 
 let myWord = 'Hello';
@@ -20,6 +20,28 @@ console.log prints original variable value 'Hello'
 string method.toUpperCase() performed returning new string (not saved)
 console.log prints original variable value 'Hello'
 
+*/
 
+/*
+
+A new variable declared with global scope occurs on line 1.
+It is initialized to a primitive string value.
+
+On line 2 the .repeat() method is invoked on the myWord variable.
+Number 3 is passed to the method as the argument for the repeat multiplier.
+Methods on string values are immutable in that they cannot be changed.
+The method returns a new string value but the value is not stored or reassigned.
+
+line 3 will therefore print the value of the myWord variable: 'Hello'.
+
+On line 4 the .replace() method is invoked on the variable.
+It seeks to replace the character 'H' from the string with the character 'J'.
+The return value from the method is a new string and this value is not saved.
+Line 5 will therefore print the value of the variable from line 1: 'Hello'.
+
+Line 6 invokes the.toUpperCase() method invocation.
+It returns a new string with the characters of the caller in uppercase.
+The value is again not stored by a variable or used.
+Line 7 should therefore print the string value: 'Hello' for the third time.
 
 */
