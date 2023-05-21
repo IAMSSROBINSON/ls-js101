ceaac7e1329facc4b84d9428152ab2524e021e96
SSR <iamssrobinson@gmail.com>
2023-05-21 18:06:23 +0100

    Further explanation of the result of example2 exercise.

---
+++ 02_small_programs/21_variables_functions_and_blocks_revisited/example_2b.js
@@ -0,0 +1,17 @@
+let myVar = [1];
+
+function myFunc () {
+  myVar = [2];
+}
+
+myFunc();
+console.log(myVar);
+
+
+/*
+
+Function invocation on line 8 for the function defined on line 3.
+The function body reassigns the global variable declared on line 1 to point to a new array at a different address in the computer memory. 
+Line 8 invokes the console.log method and passes in a reference to the pointer variable which points to the value of the array and outputs: [2] to the console.
+
+*/
