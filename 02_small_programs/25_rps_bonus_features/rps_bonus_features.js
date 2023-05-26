632f503d6c6d4ad749e8ea75af0c8895f3ce180b
SSR <iamssrobinson@gmail.com>
2023-05-26 12:50:45 +0100

    added lizard and spock choices for selection and game play: working.

--- 02_small_programs/25_rps_bonus_features/rps_bonus_features.js
+++ 02_small_programs/25_rps_bonus_features/rps_bonus_features.js
@@ -1,31 +1,48 @@
 // require the readline-sync library so we can take user input from input stream
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
 // convention to make global const variables uppercase & define top of program
 // using an array in this way allows for adding extra choices later if required
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // make output messages more distinctive
-function prompt (message) {
+function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 // get computer move, generate random number, use to select from array
-function randomNumber (min, max) {
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // winner extracted to function for better separation of concerns
-function getWinner (userMove, computerMove) {
+function getWinner(userMove, computerMove) {
   if (userMove === computerMove) {
     prompt(`The game is tied!`);
-  } else if ((userMove === 'rock' && computerMove === 'paper') ||
-      (userMove === 'paper' && computerMove === 'scissors') ||
-      (userMove === 'scissors' && computerMove === 'rock')) {
+  } else if (
+    (userMove === "rock" && computerMove === "paper") ||
+    (userMove === "paper" && computerMove === "scissors") ||
+    (userMove === "scissors" && computerMove === "rock") ||
+    (userMove === "rock" && computerMove === "lizard") ||
+    (userMove === "lizard" && computerMove === "paper") ||
+    (userMove === "lizard" && computerMove === "spock") ||
+    (userMove === "spock" && computerMove === "rock") ||
+    (userMove === "spock" && computerMove === "scissors") ||
+    (userMove === "scissors" && computerMove === "lizard") ||
+    (userMove === "paper" && computerMove === "spock")
+  ) {
     prompt(`The Computer wins!`);
-  } else if ((computerMove === 'rock' && userMove === 'paper') ||
-      (computerMove === 'paper' && userMove === 'scissors') ||
-      (computerMove === 'scissors' && userMove === 'rock')) {
+  } else if (
+    (computerMove === "rock" && userMove === "paper") ||
+    (computerMove === "paper" && userMove === "scissors") ||
+    (computerMove === "scissors" && userMove === "rock") ||
+    (computerMove === "rock" && userMove === "lizard") ||
+    (computerMove === "lizard" && userMove === "paper") ||
+    (computerMove === "lizard" && userMove === "spock") ||
+    (computerMove === "spock" && userMove === "rock") ||
+    (computerMove === "spock" && userMove === "scissors") ||
+    (computerMove === "scissors" && userMove === "lizard") ||
+    (computerMove === "paper" && userMove === "spock")) {
     prompt(`The User wins!`);
   }
 }
@@ -33,15 +50,14 @@ function getWinner (userMove, computerMove) {
 // start the program with a condition that is true
 // when we break out of this loop the program will stop
 while (true) {
-
   // ask user to choose rock, paper or scissors
   // .join() method accepts a delimiter to separate elements, returns string
-  prompt(`Please choose your move: ${VALID_CHOICES.join(', ')}:`);
+  prompt(`Please choose your move: ${VALID_CHOICES.join(", ")}:`);
   let userMove = readline.question();
 
   // if the array does not include the userMove
   while (!VALID_CHOICES.includes(userMove)) {
-    prompt(`Invalid Entry!\nPlease type: ${VALID_CHOICES.join(', ')}`);
+    prompt(`Invalid Entry!\nPlease type: ${VALID_CHOICES.join(", ")}`);
     userMove = readline.question();
   }
 
@@ -56,11 +72,11 @@ while (true) {
 
   prompt(`Do you want to play again: y/n`);
   let restartProgram = readline.question().toLowerCase();
-  while (restartProgram[0] !== 'n' && restartProgram[0] !== 'y') {
+  while (restartProgram[0] !== "n" && restartProgram[0] !== "y") {
     prompt(`Please enter 'y' or 'no'`);
     restartProgram = readline.question().toLowerCase();
   }
-  if (restartProgram[0] !== 'y') {
+  if (restartProgram[0] !== "y") {
     break;
   }
 }
