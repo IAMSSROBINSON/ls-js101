92f5cff9a9c70a386931dc75360e854acaccb4bd
SSR <iamssrobinson@gmail.com>
2023-05-29 17:40:04 +0100

    Tested score logic for best of five - status: working. Next: Do quickTrials for short hand and spock features.

--- 02_small_programs/25_rps_bonus_features/score_quickTrial_1.js
+++ 02_small_programs/25_rps_bonus_features/score_quickTrial_1.js
@@ -3,13 +3,26 @@ let computerScore = 0;
 const validMoves = ["rock", "paper", "scissors"];
 const readline = require("readline-sync");
 
-function displayWinner(userMove, computerMove) {
+function playerWins(userMove, computerMove) {
   return (
     (userMove === "rock" && computerMove === "scissors") ||
     (userMove === "paper" && computerMove === "rock") ||
     (userMove === "scissors" && computerMove === "paper")
   );
 }
+
+function displayWinner (userMove, computerMove) {
+  if (playerWins(userMove, computerMove)) {
+    userScore += 1;
+    console.log("User wins\n");
+  } else if (userMove === computerMove) {
+    console.log("Tied game\n");
+  } else {
+    computerScore += 1;
+    console.log("Computer wins\n");
+  }
+}
+
 
 while (true) {
   while (userScore < 3 && computerScore < 3) {
@@ -24,20 +37,17 @@ while (true) {
     console.log(`userMove: ${userMove}\ncomputerMove: ${computerMove}`);
     console.log("\n");
 
-    if (displayWinner(userMove, computerMove)) {
-      userScore += 1;
-      console.log("User wins\n");
-    } else if (userMove === computerMove) {
-      console.log("Tied game\n");
-    } else {
-      computerScore += 1;
-      console.log("Computer wins\n");
-    }
+    displayWinner(userMove, computerMove);
 
-    console.log(`userScore: ${userScore}\ncomputerScore: ${computerScore}`);
+    console.log(`userScore: ${userScore}\ncomputerScore: ${computerScore}\n`);
 
     let again;
     if (userScore === 3 || computerScore === 3) {
+      if (userScore === 3) {
+        console.log(`You are the Grand Winner!\nuserScore: ${userScore}\ncomputerScore: ${computerScore}\n`);
+      } else {
+        console.log(`The Computer is the Grand Winner!\ncomputerScore: ${computerScore}\nuserScore: ${userScore}\n`);
+      }
       console.log("Would you like to play again? y/n");
       again = readline.question();
 
@@ -56,19 +66,3 @@ while (true) {
     }
   }
 }
-
-
-/*
-What do you want to do?
-increase the score by +1 if a player wins that move
-
-How?
-reassign the value of the player that wins by + 1 when they win
-
-But?
-The variable values are resetting to zero each time the program restarts
-
-How will you fix it?
-loop the game restart if a score is not 3
-
-*/
